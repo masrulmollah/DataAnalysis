72b9538a7aacdef99e28820f1d951d25c8dfa080
masrulmollah <54163010+masrulmollah@users.noreply.github.com>
2026-02-17 21:16:27 +0600

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
-  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
   }
 });
