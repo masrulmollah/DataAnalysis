0e23861dd25adae34fee2110ae20da7c16285330
masrulmollah <54163010+masrulmollah@users.noreply.github.com>
2026-02-17 21:01:11 +0600

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
-  },
   build: {
     outDir: 'dist',
-    sourcemap: false,
     emptyOutDir: true
   }
 });
